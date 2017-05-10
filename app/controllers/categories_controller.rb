906e22d78120c0ee33ce3fcae23901c3b97afe5f
Andrew J Royce <andrewjroyce@gmail.com>
2017-05-10 22:09:29 +0000

    added ability to create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,8 @@
 class CategoriesController < ApplicationController
 
   def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+    @categories = Category.find(params[:category_id])
+   # @products = @category.products.order(created_at: :desc)
   end
 
 end
