8c61571cedfde2d4efcb2da032de0487d0d9b391
Andrew J Royce <andrewjroyce@gmail.com>
2017-05-11 00:30:49 +0000

    added authentication

---
+++ db/migrate/20170510233719_create_firsts.rb
@@ -0,0 +1,11 @@
+class CreateFirsts < ActiveRecord::Migration
+  def change
+    create_table :firsts do |t|
+      t.string :last
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps null: false
+    end
+  end
+end
